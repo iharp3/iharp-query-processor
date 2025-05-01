bc5ac4cb8b07fbbe22bda1ee74337659a19da790
Adil Geilani <geila005@umn.edu>
2025-05-01 13:34:41 -0500

    Implement MultiHeatmapExecutor and MultiTimeSeriesExecutor

--- src/multi_variables_executor.py
+++ src/multi_variables_executor.py
@@ -2,6 +2,8 @@ import xarray as xr
 
 from .metadata import Metadata
 from .query_executor_get_raster import GetRasterExecutor
+from .query_executor_heatmap import HeatmapExecutor
+from .query_executor_timeseries import TimeseriesExecutor
 from .utils.const import long_short_name_dict, NO_AGG_LIT
 
 
@@ -100,8 +102,86 @@ class MultiHeatmapExecutor:
             self.metadata = Metadata("/data/metadata.csv")
     
     def execute(self):
-        pass
-        # TODO
-
+        executors = [
+            HeatmapExecutor(
+                print("HeatmapExecutor is executing..."),
+                variable,
+                self.start_datetime,
+                self.end_datetime,
+                self.min_lat,
+                self.max_lat,
+                self.min_lon,
+                self.max_lon,
+                self.spatial_resolution,
+                self.heatmap_aggregation_method,
+                self.aggregation,
+                metadata=self.metadata.f_path,
+            )
+            for variable in self.variables
+        ]
+        datasets = [executor.execute() for executor in executors]
+        #test print statement for debugging
+        if not datasets:
+            print("No datasets were retrieved. Check the HeatmapExecutor.")
+        combined_datasets = xr.merge(datasets)
+        if combined_datasets is None:
+            print("xr.merge() returned None. There may be issues with the datasets.")
+        else:
+            print(f"Successfully merged datasets: {combined_datasets}")
+        return combined_datasets
+        
 class MultiTimeSeriesExecutor:
+    def __init__(
+        self,
+        variable: str,
+        start_datetime: str,
+        end_datetime: str,
+        min_lat: float,
+        max_lat: float,
+        min_lon: float,
+        max_lon: float,
+        temporal_resolution: str,  # e.g., "hour", "day", "month", "year"
+        aggregation,  # e.g., "mean", "max", "min"
+        time_series_aggregation_method: str,  # e.g., "mean", "max", "min"
+        metadata=None,  # metadata file path
+    ):
+        #getter variables
+        self.variable = variable
+        self.start_datetime = start_datetime
+        self.end_datetime = end_datetime
+        self.min_lat = min_lat
+        self.max_lat = max_lat
+        self.min_lon = min_lon
+        self.max_lon = max_lon
+        self.temporal_resolution = temporal_resolution
+        self.aggregation = aggregation
+        self.time_series_aggregation_method = time_series_aggregation_method
+        #Query the internal variables
+        self.variable_short_names = [long_short_name_dict[v] for v in variable]
+        if metadata:
+            self.metadata = Metadata(metadata)
+        else:
+            self.metadata = Metadata("/data/metadata.csv")
+
+    def execute(self):
+        executors = [
+            TimeseriesExecutor(
+                variable_2,
+                self.start_datetime,
+                self.end_datetime,
+                self.min_lat,
+                self.max_lat,
+                self.min_lon,
+                self.max_lon,
+                self.temporal_resolution,
+                self.time_series_aggregation_method,
+                self.aggregation,
+                metadata=self.metadata.f_path,
+            )
+            for variable_2 in self.variable
+        ]
+        datasets = [executor.execute() for executor in executors]
+        combined_dataset = xr.merge(datasets)
+        return combined_dataset
+        
     pass
